133043efd79cd76536291df5d42a2df6fb4d6e00
Raziel Ventura <ethush@gmail.com>
2021-02-28 18:57:11 -0600

    Update getJson_to_ext_var.js
    Added comments

--- getJson_to_ext_var.js
+++ getJson_to_ext_var.js
@@ -1,9 +1,13 @@
+/**
+ * Get the values from a URL using Ajax and return them to an external variable for further processing.
+ */
+
 var json = (function () {
     var json = null;
         $.ajax({
             'async': false,
             'global': false,
-            'url': mapas[j],
+            'url': $the_url,
             'dataType': "json",
             'success': function (data) {
                    json = data;
